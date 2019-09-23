37c8085ae0ca8e1a3a0c2c0f52f56de0d999c4e8
MrvnDmn <52141545+MrvnDmn@users.noreply.github.com>
2019-09-24 02:21:38 +0800

    Add files via upload

---
+++ reaction.php
@@ -0,0 +1,186 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="index.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<title>Reaction Papers</title>
+</head>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img class="profile" src="Profile.jpg" /></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Home<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Links
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="https://github.com/MrvnDmn">Github</a>
+          <a class="dropdown-item" href="https://facebook.com/MrvnDmn9">Facebook</a>
+		  <div class="dropdown-divider"></div>
+		  <span class="dropdown-item">APC Email - msdimaano@student.apc.edu.ph</span>
+		  <span class="dropdown-item">Gmail - md.dimaano00@gmail.com</span>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Class Works
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="activities.php">Activities  |  A1, A2, A3, A7</a>
+          <a class="dropdown-item" href="reaction.php">Reaction Papers</a>
+		  <a class="dropdown-item" href="group.php">Group Works  |  A4, A5, A6</a>
+		  <a class="dropdown-item" href="user.php">User Inyerface Quiz</a>
+		  <a class="dropdown-item" href="learnings.php">Certificates</a>
+        </div>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="https://l1-blog.herokuapp.com/">Lab 1</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<body style="background-color: black;">
+<center><h1 class="headerfront">REACTION PAPERS</h1></center>
+<div class="accordion" id="accordionExample" style="margin-top: 100px;">
+  <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+          Reaction #1: First Week of User Design
+        </button>
+      </h5>
+    </div>
+    <div id="collapseOne" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+	  The first week of User Design was not what I expected, there were a lot more on the essay works rather than I expected.
+	  It’s not just what I had in mind when entering the first week, I thought there would be more programming.
+	  I get why we had to that, for me at least, it teaches me the basic concept of design, conceptualizing the idea without having to code it,
+	  which may change because there are some errors that might interfere with design that I had in mind.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingTwo">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Reaction #2: Need Finding and Paper Prototyping
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+	  The 2<sup>nd</sup> week of USERDES class was more fun than the previous week. Especially when we are
+working on designing a website and/or an app. It was something that I really find enjoyable at
+programming, designing, especially when I was able to work with others and see their views on design.
+We did this activity on the topic of Paper Prototyping. We also got to use the Microsoft Room which was
+different than usual.
+<br><br>
+For the topic of need finding, I was able to understand that you can’t just make an app or
+website or a device just instantly because you think it’s a good idea. You need to cater the needs of
+others. You need to answer the “who’s going to use it”, “why would they use it”, “what is the purpose of
+it”. There are no steps to skip and you need to carefully think out a plan to execute. The videos that was
+shown explained the topic of Need Finding, especially the video about the shopping cart.
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header" id="headingThree">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Reaction #3: Lab 1: Heroku and Github
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+      <div class="card-body">
+	  For the third week, I quite enjoyed the entire website developing process. HTML was the
+language that really pushed me to take the Computer Science course. So, going back to the language
+again was fun. I must admit I did forget a lot about the language that’s why I was having a little trouble
+when programming the blog site. 
+<br><br>
+For the website that I created it was quite a boring and plain site.
+There’s not really much interaction with user, it’s a simple “read only” blog. I do plan on adding more to
+the site as it is required.
+<br><br>
+Probably the most confusing part of the week was when we were submitting
+the website. We used Heroku and Github, which I am not familiar with, and there was a bit of trouble
+when submitting, which was easily resolved, we just have to change the HTML to PHP, but when I was
+submitting mine, I had no idea how to connect the Github and Heroku and to properly launch the my
+project to Heroku, but I was shown how to do it, and I feel stupid not knowing something that is quite
+simple.
+      </div>
+    </div>
+</div>
+ <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Reaction #4: Userinyerface
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFour" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+	  We had to take a “UX Quiz”, it was this annoying test website with a lot of difficult and poorly
+designed website. The game was annoying because there were a lot of simple things that will you miss,
+and it will affect the entire quiz. That’s what made the site annoying, you think that you might be
+finished but you click or type something and everything is changed. That annoying part is what also what
+made the website fun in some way, there was a timer and you can see how well you got through the
+entire site. 
+	  </div>
+    </div>
+</div>
+ <div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+          Reaction #5: SoCIT Fest
+        </button>
+      </h5>
+    </div>
+    <div id="collapseFive" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+	  The SoCIT Fest was, well, it was a bit boring. Almost every fest at APC greets the student on the first day with a decorated ground floor,
+	  but during the first day some were just beginning to decorate the ground floor. The SoCIT fest was filled with seminars, and I know some people really
+	  hate seminars. I kinda feel bad for some of the speakers during the seminar the happened in the auditorium, because some students left early and it almost
+	  emptied the auditorium. The SoCIT fest, thought it had some fun events, like the UI Design competition, which I forgot to join, it really was forgettable for some 
+	  of the 2nd year students. I don't know for the first year students, which I know had more events. The SoCIT fest in its entirety, was boring and if I would give it 
+	  a rating it would be 5/10 
+	  </div>
+    </div>
+</div>
+<div class="card">
+    <div class="card-header" id="headingOne">
+      <h5 class="mb-0">
+        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+          Reaction #6: First Half of User Design
+        </button>
+      </h5>
+    </div>
+    <div id="collapseSix" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+      <div class="card-body">
+	  For the first half of User Design it was definitely now what I wanted it to be. Coming in to User Design class, I thought that we would learn the basics
+	  of GUI which was something that was not taught to us the previous year. So being able to apply that would be benefitial to us.
+	  
+	  The challenge was really adjusting back to HTML, I've used HTML before, it was the language that got me into programming but I haven't used in about 2 years.
+	  So I had to learn all the basics and what some of the tags do and what some of the CSS functions were. It was fun learning all of it again.
+	  
+	  For the final half of User Design I expect it to be more challenging because we have to finish up our project. One more challenge is the paper, because almost
+	  every subject during our term requires us to create a research paper of a project. The addition of one more researh paper would be a challenge especially if me and my 
+	  group are having trouble with our topic.
+      </div>
+	</div>
+</div>
+	</div>
+</body>
+</html>
